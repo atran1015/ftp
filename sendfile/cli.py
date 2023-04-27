6b027e1e09fcca3436b2952108051e7da521cea3
rocketmanZ <rocketmanz@live.com>
2023-04-26 17:35:49 -0700

    fix/refractor

--- sendfile/cli.py
+++ sendfile/cli.py
@@ -6,37 +6,54 @@ import subprocess
 
 
 def SendData(filename):
-    fileObj = open(filename, "rb")
-    fileData = fileObj.read(65536)
-    if fileData:
-        # print(fileData)
-    	# Get the size of the data read
-		# and convert it to string
-        dataSizeStr = str(len(fileData))
-        # print(type(fileData))
-		# Prepend 0's to the size string
-		# until the size is 10 bytes
-        while len(dataSizeStr) < 10:
-            dataSizeStr = "0" + dataSizeStr
-	
-	
-		# Prepend the size of the data to the
-		# file data.
-
-        # convert bytes to str
-        fileData = dataSizeStr + str(fileData)	
-		
-		# The number of bytes sent
+    try:
+        fileObj = open(filename, "rb")
+        fileSize = os.path.getsize(filename)
         numSent = 0
-		#print("number of bytes", fileData[numSent:].encode()) #this is sending 0000000022This is a test string
-		
-		# Send the data!
-        while len(fileData) > numSent:
-            numSent += connSock.send(fileData[numSent:].encode()) #this is sending bytes-object
-	
-	# The file has been read. We are done
-    fileObj.close()
-    return numSent
+        while numSent < fileSize:
+            fileData = fileObj.read(65536)
+            if not fileData:
+                break;
+        #if fileData:
+            # print(fileData)
+            # Get the size of the data read
+            # and convert it to string
+            dataSizeStr = str(len(fileData)).zfill(10)
+            fileData = dataSizeStr.encode() + fileData
+            # print(type(fileData))
+            # Prepend 0's to the size string
+            # until the size is 10 bytes
+            #while len(dataSizeStr) < 10:
+            #    dataSizeStr = "0" + dataSizeStr
+            
+        
+            # Prepend the size of the data to the
+            # file data.
+
+            # convert bytes to str
+            #fileData = dataSizeStr + str(fileData)	
+            
+            # The number of bytes sent
+            #numSent = 0
+            #print("number of bytes", fileData[numSent:].encode()) #this is sending 0000000022This is a test string
+            
+            # Send the data!
+            while fileData:
+                sent = connSock.send(fileData)
+                if not sent:
+                    raise RuntimeError("Socket connection interuppted")
+                fileData = fileData[sent:]
+                numSent += sent
+            #while len(fileData) > numSent:
+             #   numSent += connSock.send(fileData[numSent:].encode()) #this is sending bytes-object
+        return numSent
+    except Exception as e:
+        print("Error when seding file: " , str(e))
+        
+        
+    except IOError as e:
+        print("Error:", e)
+        return 0
         
 
 # Command line checks 
@@ -62,7 +79,12 @@ serverPort = int(sys.argv[2])
 connSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server
-connSock.connect((serverAddr, serverPort))
+try:
+    connSock.connect((serverAddr, serverPort))
+
+except socket.error as e:
+    print("Error: Connection failed: ", e)
+    sys.exit()
 
 # The number of bytes sent
 numSent = 0
