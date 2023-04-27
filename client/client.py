a5598d2ac72c8e77d553246e96b7830009febfb8
rocketmanZ <rocketmanz@live.com>
2023-04-26 23:17:59 -0700

    error handling

--- client/client.py
+++ client/client.py
@@ -9,65 +9,116 @@ if len(sys.argv) != 3:
 server_host = sys.argv[1]
 server_port = int(sys.argv[2])
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as e:
+    print("Error creating socket:", e)
+    sys.exit()
 
 # connect server
-client_socket.connect((server_host, server_port))
+try:
+    client_socket.connect((server_host, server_port))
+except socket.error as e:
+    print("Error connecting to server:", e)
+    client_socket.close()
+    sys.exit()
 
 while True:
 
+    try:
+        command = input("ftp> ")
+    except KeyboardInterrupt:
+        print("Interrupted.")
+        client_socket.close()
+        sys.exit()
 
-    command = input("ftp> ")
-    
     command_list = command.split()
     cmd = command_list[0]
-    
+
     if cmd == 'quit':
-        client_socket.sendall('quit'.encode())
+        try:
+            client_socket.sendall('quit'.encode())
+        except socket.error as e:
+            print("Error sending command:", e)
+            client_socket.close()
+            sys.exit()
         break
-    
+
     elif cmd == 'get':
-        filename = command_list[1]
-        client_socket.sendall(f'get {filename}'.encode())
-        
-        filesize = int(client_socket.recv(1024).decode())
-        
-        with open(f'received_{filename}', 'wb') as file:
-            received = 0
-            while received < filesize:
-                data = client_socket.recv(1024)
-                received += len(data)
-                file.write(data)
-                
+        try:
+            filename = command_list[1]
+            client_socket.sendall(f'get {filename}'.encode())
+        except (IndexError, socket.error) as e:
+            print("Error sending command:", e)
+            continue
+
+        try:
+            filesize = int(client_socket.recv(1024).decode())
+        except (ValueError, socket.error) as e:
+            print("Error receiving filesize:", e)
+            continue
+
+        try:
+            with open(f'received_{filename}', 'wb') as file:
+                received = 0
+                while received < filesize:
+                    data = client_socket.recv(1024)
+                    if not data:
+                        break
+                    received += len(data)
+                    file.write(data)
+
             print(f'Download complete. {filesize} bytes received.')
-        
+        except (OSError, socket.error) as e:
+            print("Error saving file:", e)
+            continue
+
     elif cmd == 'put':
-        filename = command_list[1]
-        client_socket.sendall(f'put {filename}'.encode())
-        
+        try:
+            filename = command_list[1]
+            client_socket.sendall(f'put {filename}'.encode())
+        except (IndexError, socket.error) as e:
+            print("Error sending command:", e)
+            continue
+
         if not os.path.exists(filename):
             print(f"File '{filename}' does not exist.")
             continue
-        
-        filesize = os.path.getsize(filename)
-        
-        client_socket.send(str(filesize).encode())
-        
-        with open(filename, 'rb') as file:
-            data = file.read(1024)
-            while data:
-                client_socket.send(data)
-                data = file.read(1024)
-                
-            print(f'Upload complete. {filesize} bytes sent.')
-        
-    elif cmd == 'ls':
-        client_socket.sendall('ls'.encode())
 
-        data = client_socket.recv(1024).decode()
-        print(data)
-    
+        try:
+            filesize = os.path.getsize(filename)
+            client_socket.send(str(filesize).encode())
+        except (OSError, socket.error) as e:
+            print("Error getting filesize:", e)
+            continue
+
+        try:
+            with open(filename, 'rb') as file:
+                data = file.read(1024)
+                while data:
+                    client_socket.send(data)
+                    data = file.read(1024)
+
+            print(f'Upload complete. {filesize} bytes sent.')
+        except (OSError, socket.error) as e:
+            print("Error reading file:", e)
+            continue
+
+    elif cmd == 'ls':
+        try:
+            client_socket.sendall('ls'.encode())
+        except socket.error as e:
+            print("Error sending command:", e)
+            continue
+
+        try:
+            data = client_socket.recv(1024).decode()
+            print(data)
+        except socket.error as e:
+            print("Error receiving data:", e)
+            continue
+
     else:
         print(f"Invalid command: {cmd}")
-        
+
 client_socket.close()
--- server/server.py
+++ server/server.py
@@ -9,8 +9,9 @@ def send_file(conn, filename):
     with open(filename, 'rb') as file:
         data = file.read(1024)
         while data:
-            conn.send(data)
-            data = file.read(1024)
+            sent = conn.send(data)
+            data = file.read(1024 - sent)
+        print(f"File '{filename}' sent.")
 
 def receive_file(conn, filename):
     filesize = int(conn.recv(1024).decode())
@@ -22,41 +23,47 @@ def receive_file(conn, filename):
             data = conn.recv(1024)
             received += len(data)
             file.write(data)
+        print(f"File '{filename}' received.")
 
 def main(port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('', port))
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(('', port))
+        server_socket.listen(1)
 
-    server_socket.listen(1)
+        # wait for connection
+        print('Waiting for a client to connect...')
+        conn, address = server_socket.accept()
+        print(f'Connection established with {address}')
 
-    # wait for connection
-    
-    print('Waiting for a client to connect...')
-    conn, address = server_socket.accept()
+        while True:
+            command = conn.recv(1024).decode()
 
-    print(f'Connection established with {address}')
+            if command.startswith('get'):
+                filename = command.split(' ')[1]
+                try:
+                    send_file(conn, filename)
+                except FileNotFoundError:
+                    conn.send(b'File not found.')
+                except Exception as e:
+                    conn.send(str(e).encode())
+            elif command.startswith('put'):
+                filename = command.split(' ')[1]
+                try:
+                    receive_file(conn, filename)
+                except Exception as e:
+                    conn.send(str(e).encode())
+            elif command == 'ls':
+                file_list = os.listdir('.')
+                conn.send('\n'.join(file_list).encode())
+            elif command == 'quit':
+                break
 
-    while True:
-        command = conn.recv(1024).decode()
-
-        if command.startswith('get'):
-            filename = command.split(' ')[1]
-            send_file(conn, filename)
-
-            print(f'Sent {filename} to client')
-        elif command.startswith('put'):
-            filename = command.split(' ')[1]
-            receive_file(conn, filename)
-
-            print(f'Received {filename} from client')
-        elif command == 'ls':
-            file_list = os.listdir('.')
-            conn.send('\n'.join(file_list).encode())
-        elif command == 'quit':
-            break
-
-    conn.close()
-    server_socket.close()
+        conn.close()
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        server_socket.close()
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
